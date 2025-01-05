a6b3e6d06658ac31430036f34539426bf6e75d92
Francisco Silva <1220870@isep.ipp.pt>
2025-01-05 23:13:09 +0000

    ARQCP Final

--- Sprint3/ARQCP/usac07/asm.h
+++
@@ -1,8 +0,0 @@
-#ifndef ASM_H
-#define ASM_H
-
-int dequeue_value(int* buffer, int length, int* tail, int* head, int *value);
-int get_n_element(int* buffer, int length, int* tail, int* head);
-
-#endif
-
--- Sprint3/ARQCP/usac10/asm.h
+++
@@ -1,7 +0,0 @@
-#ifndef ASM_H
-#define ASM_H
-
-extern int median(int* vec, int length, int *me);
-
-#endif
-	
